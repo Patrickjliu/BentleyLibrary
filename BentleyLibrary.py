2b1ce3373eb4dd0fa268c8b2530fda266c022496
Patrickjliu <liujpatrick@gmail.com>
2023-05-12 17:13:53 -0700

    Worked on django, added search, and refined code

--- BentleyLibrary.py
+++ BentleyLibrary.py
@@ -3,235 +3,321 @@ import datetime
 import time
 import mysql.connector
 import os
-from dotenv import load_dotenv
 from PIL import Image
 import io
-
-load_dotenv()
-
-#Credentials
-
-#test
-# host = os.environ.get('DB_HOST')
-
-# if host is None:
-#     print("Environment variable 'host' is not set.")
-# else:
-#     print("Environment variable 'host' is set to:", host)
-
-mydb = mysql.connector.connect(
-  host=os.getenv('DB_HOST'),
-  user=os.getenv('DB_USER'),
-  password=os.getenv('DB_PASSWORD'),
-  database=os.getenv('DB_NAME'),
-)
-
-name = ""
-lastname = ""
-email = ""
-year = 00
-checkoutPeriod = 14
-
-# Get the current time
-current_time = datetime.datetime.now()
-
-# Format the current date as a string
-current_time_str = current_time.strftime('%Y-%m-%d')
-
-# Add the checkout period to the current datetime object
-due_date = current_time + datetime.timedelta(days=checkoutPeriod)
-
-# Format the due date as a string
-due_date_str = due_date.strftime('%Y-%m-%d')
-
-# Create a cursor object
-cursor = mydb.cursor()
-
-# Prepare the SQL query to insert data
-mycursor = mydb.cursor()
-
-# Insert data into the table
-sql = "INSERT INTO yourtable (Title, Author, Publication_Date, ISBN, User_First_Name, User_Last_Name, User_Email, User_Grad_Year, Number_of_Current_Book_Checked_Out, Due_Date, Status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-
-def getTitle(ISBN):
-  response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q=isbn:{ISBN}")
-  data = response.json()
-  title = data["items"][0]["volumeInfo"]["title"]
-  return title
-
-def getAuthor(ISBN):
-  response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q=isbn:{ISBN}")
-  data = response.json()
-  author = data["items"][0]["volumeInfo"]["authors"]
-  authorString = ', '.join(author)
-  return authorString
-
-def getPubDate(ISBN):
-  response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q=isbn:{ISBN}")
-  data = response.json()
-  date = data["items"][0]["volumeInfo"]["publishedDate"]
-  return date
-
-def getThumbnail(ISBN):
-  response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q=isbn:{ISBN}")
-  data = response.json()
-  date = data["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
-  return date
-
-def validyn(var, prompt):
-  while True:
-    
-    Ans = input(f"Is this correct? (y/n) ").lower()
-    if Ans == "n":
-      var = input(prompt)
-    elif Ans == "y":
-      return var
-    else:
-      print("Invalid Entry")
-      while Ans != "y" or Ans != "n":
-        Ans = input(str).lower()
-        if Ans == "n":
-          var = input(prompt)
-        elif Ans == "y":
-          return var
-
-def validString(str):
-  if not str.isalpha():
-    # print(not str.isaplha(), str)
-    print("Please enter letters only.")
-    return False
-  elif len(str) <= 1:
-    print("Name must be more that one letter")
-    return False
-  return True
-
-def login():
-  print("Hello. Welcome to Bentley's Libary Check In and Check Out System! Press Enter/Return to Continue.")
-  
-  name = input("What is your first name?\n").lower()
-  
-  while not validString(name):
-    name = input("What is your first name?\n").lower()
-    print(name)
-    
-  lastname = input("What is your last name?\n").lower()
-  
-  while not validString(lastname):
-      lastname = input("What is your last name?\n").lower()
-      print(lastname)
-  
-  year = input("What year do you graduate? Please enter your year in the two digit format. Ex. 25\n")
-  while True:
-    if year.isalpha():
-      print("You should not have letters in your year.")
-    elif len(year) != 2:
-      print("Year should be two numbers.")
-    else: break
-    year = input("What year do you graduate? Please enter your year in the two digit format. Ex. 25\n")
-
-  email = name[0] + lastname + year + "@bentleyschool.org"
-  print(f"Is you email: {email}? ")#If yes, enter y. If no, enter n.")
-  emailPrompt = "What is your email?"
-  validyn(email, emailPrompt)
-  print("fixed" + email)
-  os.system("clear")
-  return name, lastname, email, year
+from Credentials import db_config
+from prettytable import PrettyTable
 
 def main():
-  
-  name, lastname, email, year = login()
-  print(f"Welcome {name}!")
-  
-  while True:
-    # Prompt the user to enter their name and action (check-in or check-out)
-    action = input("Would you like to check in or check out a book? Enter ci for check in and co for check out. ")
+  mydb = mysql.connector.connect(**db_config)
 
+  name = ""
+  lastname = ""
+  email = ""
+  year = 00
+  ISBN = ""
+  checkoutPeriod = 14
+  inInventory = False
+  excute = False
+  newavailable_quantity = 0
+  id = 0
 
+  # Create a cursor object
+  cursor = mydb.cursor()
 
-    # If the user is checking in, add their name and check-in time to the dictionary
-    if action == "ci":
+  # Prepare the SQL query to insert data
+  mycursor = mydb.cursor()
+
+  # Insert data into the table
+  sql = "INSERT INTO log (title, author, publisher, publication_year, borrower_first_name, borrower_last_name, borrowed_date) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+
+  def list_to_string(lst):
+      if len(lst) == 1:
+          return str(lst[0])
+      else:
+          return ', '.join(map(str, lst))
+
+  def getTitle(ISBN):
+    response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q=isbn:{ISBN}")
+    data = response.json()
+    title = data["items"][0]["volumeInfo"]["title"]
+    return title
+
+  def getAuthor(ISBN):
+    response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q=isbn:{ISBN}")
+    data = response.json()
+    author = data["items"][0]["volumeInfo"]["authors"]
+    authorString = list_to_string(author)
+    return authorString
+
+  def getPub(ISBN):
+    response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q=isbn:{ISBN}")
+    data = response.json()
+    pub = data["items"][0]["volumeInfo"]["publisher"]
+    return pub
+
+  def getPubDate(ISBN):
+    response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q=isbn:{ISBN}")
+    data = response.json()
+    pub_date_str = data["items"][0]["volumeInfo"]["publishedDate"]
+    
+    # Attempt to parse the publication date string
+    try:
+      pub_date = datetime.datetime.strptime(pub_date_str, '%Y-%m-%d').date()
+    except ValueError:
+      # If parsing fails, use a default date of 1900-01-01
+      pub_date = datetime.date(1900, 1, 1)
+    
+    return str(pub_date)
+
+  def getThumbnail(ISBN):
+    response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q=isbn:{ISBN}")
+    data = response.json()
+    thumbnail = data["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
+    return thumbnail
+
+  def validyn(var, prompt):
+    while True:
+      Ans = input(f"Is this correct? (y/n) ").lower()
+      if Ans == "n":
+        var = input(prompt)
+      elif Ans == "y":
+        return var
+      else:
+        print("Invalid Entry")
+        while Ans != "y" or Ans != "n":
+          Ans = input(Ans).lower()
+          if Ans == "n":
+            var = input(prompt)
+          elif Ans == "y":
+            return var
+
+  def validString(str):
+    if not str.isalpha():
+      # print(not str.isaplha(), str)
+      print("Please enter letters only.")
+      return False
+    elif len(str) <= 1:
+      print("Name must be more that one letter")
+      return False
+    return True
+
+  def login():
+    print("Hello. Welcome to Bentley's Libary Check In and Check Out System!")
+    
+    name = input("What is your first name?\n").lower()
+    
+    while not validString(name):
+      name = input("What is your first name?\n").lower()
+      print(name)
       
+    lastname = input("What is your last name?\n").lower()
+    
+    while not validString(lastname):
+        lastname = input("What is your last name?\n").lower()
+        print(lastname)
+    
+    year = "20" + input("What year do you graduate? Please enter your year in the two digit format. Ex. 25\n")
+    while True:
+      if year.isalpha():
+        print("You should not have letters in your year.")
+      elif len(year) != 4:
+        print("Year should be two numbers.")
+      else: break
+      year = "20" + input("What year do you graduate? Please enter your year in the two digit format. Ex. 25\n")
+
+    email = name[0] + lastname + year[2:] + "@bentleyschool.org"
+    print(f"Is this you email: {email}? ")#If yes, enter y. If no, enter n.")
+    emailPrompt = "What is your email?"
+    email = validyn(email, emailPrompt)
+    print("fixed" + email)
+    os.system("clear")
+    return name.capitalize(), lastname.capitalize(), email, year
+
+  def cico():
+    global inInventory, excute, ISBN, newavailable_quantity, id
+    name, lastname, email, year = login() #"pat", "liu", "pliu25@bentleyschool.org", "2025" 
+    name = name.capitalize()
+    lastname.capitalize()
+    print(f"Welcome {name}!")
+    
+    while True:
       
       ISBN = input("Please find and enter the ISBN of the book on the back cover, near the barcode. ")
       while True:
         try: 
-      
-          print(f"Is '{getTitle(ISBN)}' the book you are checking in? If yes, enter y. If no, enter n. ")
-          response = requests.get(getThumbnail(ISBN))
-          img = Image.open(io.BytesIO(response.content))
-          img.show()
-          time.sleep(2)
-          img.close()
-          validyn(ISBN, "Please enter the ISBN. ")
-          break
-        except:
-          print("Invalid ISBN. ")
-          ISBN = input("Please enter the ISBN. ")
-
-      value = (getTitle(ISBN), getAuthor(ISBN), getPubDate(ISBN), ISBN, name, lastname, email, year, "1", current_time_str, "Checked In")
-      
-      mycursor.execute(sql, value)
-          
-      # check_in_out = {"name": name, "ISBN": ISBN, "check-in": current_time}
-      # db[name] = check_in_out
-      # print(db[name])
-      # print(f"{name} has checked in {getTitle(ISBN)} at {current_time}")
-      print(f"{name} has checked out {getTitle(ISBN)} at {current_time}")
-      print("Thank you for checking out a book.")
-    # If the user is checking out, add their check-out time to the dictionary
-    elif action == "co":
-        # Check if the user has checked in before
-        ISBN = input("Please find and enter the ISBN of the book on the back cover, near the barcode. ")
-        while True:
-          try: 
-            print(f"Is '{getTitle(ISBN)}' the book you are checking out? ")
-            
-            validyn(ISBN, "Please enter the ISBN. ")
+          print(f"Is '{getTitle(ISBN)}' the book? ")
+          try:
             response = requests.get(getThumbnail(ISBN))
             img = Image.open(io.BytesIO(response.content))
             img.show()
             time.sleep(2)
             img.close()
-            break
+          
           except:
-            print("Invalid ISBN. ")
-            ISBN = input("Please enter the ISBN. ")
-
+            pass
+          
+          ISBN = validyn(ISBN, "Please enter the ISBN. ")
+          
+          break
+        except:
+          print("Invalid ISBN. ")
+          ISBN = input("Please enter the ISBN. ")
         
-        value = (getTitle(ISBN), getAuthor(ISBN), getPubDate(ISBN), ISBN, name, lastname, email, year, "1", due_date_str, "Checked Out")
-        for v in value:
-          print(v)
-          print(type(v))
+      getaval = "SELECT id, quantity, available_quantity FROM bookinventory WHERE isbn = %s"
+      cursor.execute(getaval, (ISBN,))
+      quanitiyResult = cursor.fetchone()
+      
+      if not quanitiyResult:
+        print("Book not found in inventory.")
+        inInventory = False
+      else:
+        inInventory = True
+        id, quantity, newavailable_quantity = quanitiyResult
+      
+      # Prompt the user to enter their name and action (check-in or check-out)
+      if inInventory:
+        
+        while True:
+          action = input("\nWould you like to check in or check out a book? Enter ci for check in and co for check out. Enter q to quit. ").lower()
 
-        mycursor.execute(sql, value)    
-        #val = ("The Great Gatsby", "F. Scott Fitzgerald", "1925-04-10", 9780684830421, "John", "Doe", "johndoe@gmail.com", 2023, 1, "2023-05-01")
-        print(f"{name} has checked out {getTitle(ISBN)} at {current_time}")
-        print("Thank you for checking out a book.")
-    # If the user enters an invalid action, prompt them to try again
+          if action == "ci":
+            # get current date as string
+            current_date = datetime.date.today().strftime('%Y-%m-%d')
+
+            # get current time as string
+            current_time = datetime.datetime.now().strftime('%H:%M:%S')
+            
+            returned = "UPDATE log SET returned_date = %s, returned_time = %s WHERE borrower_email = %s AND isbn = %s AND returned_date IS NULL AND returned_time IS NULL ORDER BY borrowed_date DESC, borrowed_time DESC LIMIT 1"
+            returnedvals = (current_date, current_time, email, ISBN)
+            
+            try: 
+              mycursor.execute(returned, returnedvals) 
+              newavailable_quantity += 1
+              if newavailable_quantity > quantity:
+                newavailable_quantity = quantity
+              print(f"{name} has checked in {getTitle(ISBN)} at {current_time}.")
+              print("Thank you for checking in a book. Please place the book into the bin. ")
+              excute = True
+            except:
+              print("Value type error. Please try again. If this issue continues, please contact Patrick.")
+                
+          elif action == "co":
+            if newavailable_quantity > 0:
+              newavailable_quantity -= 1
+            else:
+              print("No copies available to check out")
+              continue
+            
+            # get current date as string
+            current_date = datetime.date.today().strftime('%Y-%m-%d')
+
+            # get current time as string
+            current_time = datetime.datetime.now().strftime('%H:%M:%S')
+            
+            borrow = "INSERT INTO log (book_id, title, author, publisher, publication_date, isbn, borrower_first_name, borrower_last_name, borrower_email, borrowed_date, borrowed_time) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+            borrowValues = (id, getTitle(ISBN), getAuthor(ISBN), getPub(ISBN), getPubDate(ISBN), ISBN, name, lastname, email, current_date, current_time) 
+
+            try: 
+              mycursor.execute(borrow, borrowValues) 
+              
+              print(f"{name} has checked out {getTitle(ISBN)} at {current_time}")
+              print("Thank you for checking out a book.")
+              excute = True
+            except:
+              print("Value type error. Please try again. If this issue continues, please contact Patrick.")
+            
+            
+              
+          # If the user enters an invalid action, prompt them to try again
+          else:
+            if action == "q":
+              return
+            print("Invalid action. Please enter 'ci' or 'co'.")
+            # Prompt the user if they want to continue or exit the program
+            exit_choice = input("Do you want to exit? Enter 'y' or 'n'. ").lower()
+            if exit_choice == "y":
+              return
+
+      # Prompt the user if they want to continue or exit the program
+      exit_choice = input("Do you want to exit? Enter 'y' or 'n'. ").lower()
+      if exit_choice == "y":
+        return
+
+  def search(sStr):
+    mycursor.execute("SELECT title, author, isbn, published_date, publisher, available_quantity FROM bookinventory WHERE description LIKE %s OR author LIKE %s OR title LIKE %s OR publisher LIKE %s", ('%' + sStr + '%', '%' + sStr + '%', '%' + sStr + '%', '%' + sStr + '%'))
+    results = mycursor.fetchall()
+    table = PrettyTable(['Title', 'Author', 'ISBN', 'Published Date', 'Publisher', 'Available Quantity'])
+      
+    # Add rows to the table
+    for row in results:
+        table.add_row(row)
+    
+    # Print the table
+    print(table)
+    input("Press Enter to continue.")
+    main()  
+
+  def main():
+    os.system('clear')
+    menu = input("Please select from the following options.\n\n1. Search \n2. Check In & Check out\n3. Quit\n")
+    if menu == "1":
+      os.system('clear')
+      search(input("What would you like to search for?\n"))
+    elif menu == "2":
+      os.system('clear')
+      cico()
+    elif menu == "3":
+      os.system('clear')
+      print("\nThank you for coming! Please contact Patrick if you have any questions.")
+      time.sleep(20)
+      os.system('clear')
     else:
-        print("Invalid action. Please enter 'ci' or 'co'.")
+      print("Invalid option")
+      main()
+      
+  main()
 
-    # Prompt the user if they want to continue or exit the program
-    exit_choice = input("Do you want to exit? Enter 'y' or 'n'. ").lower()
-    if exit_choice == "y":
-      break
+  if excute:
+    try:
+      # Commit the changes to the database
+      mydb.commit()
 
+      # check if the insertion was successful by verifying the number of affected rows
+      # if mycursor.rowcount > 0:
+      #   print("Insertion successful!")
+      # else:
+      #   print("Insertion failed. Please try again. If this issue continues, please contact Patrick.")
+      #   while True:
+      #     exit_choice = input("Do you want to exit? Enter 'y' or 'n'. ").lower()
+      #     if exit_choice == "y":
+      #       break
+      #     else:
+      #       main()
+
+      try:
+        update = "UPDATE bookinventory SET available_quantity = %s WHERE id = %s"
+        cursor.execute(update, (newavailable_quantity, id))
+        mydb.commit()
+      except:
+        print("Failed to update quanitiy in inventory. Please contact Patrick.")
+
+      # Close the connection
+      mydb.close()
+    except:
+      pass
+
+  def printall(table):
+    # Execute the SELECT query
+    query = "SELECT * FROM %s"
+    mycursor.execute(query, (table,))
+
+    # Fetch all rows from the result set
+    rows = mycursor.fetchall()
+
+    # Print each row
+    for row in rows:
+      print(row)
+      
 main()
-
-# Commit the changes to the database
-mydb.commit()
-
-# Close the connection
-print(mycursor.rowcount, "record inserted.")
-mydb.close()
-
-def printall():
-  # Execute the SELECT query
-  mycursor.execute("SELECT * FROM new_table")
-
-  # Fetch all rows from the result set
-  rows = mycursor.fetchall()
-
-  # Print each row
-  for row in rows:
-    print(row)
---
+++ BentleyLibraryAdmin.py
@@ -0,0 +1,177 @@
+import requests
+from tkinter import *
+from tkinter import filedialog
+from PIL import Image, ImageTk
+import datetime
+import io
+import pandas as pd
+from openpyxl import Workbook
+import xlsxwriter
+import mysql.connector
+from Credentials import db_config
+
+mydb = mysql.connector.connect(**db_config)
+
+def list_to_string(lst):
+    if len(lst) == 1:
+        return str(lst[0])
+    else:
+        return ', '.join(map(str, lst))
+
+def getTitle(ISBN):
+    response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q=isbn:{ISBN}")
+    data = response.json()
+    title = data["items"][0]["volumeInfo"]["title"]
+    return title
+
+def getAuthor(ISBN):
+  response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q=isbn:{ISBN}")
+  data = response.json()
+  author = data["items"][0]["volumeInfo"]["authors"]
+  authorString = list_to_string(author)
+  return authorString
+
+def getPub(ISBN):
+  response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q=isbn:{ISBN}")
+  data = response.json()
+  pub = data["items"][0]["volumeInfo"]["publisher"]
+  return pub
+
+def getPubDate(ISBN):
+  response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q=isbn:{ISBN}")
+  data = response.json()
+  pub_date_str = data["items"][0]["volumeInfo"]["publishedDate"]
+  
+  # Attempt to parse the publication date string
+  try:
+    pub_date = datetime.datetime.strptime(pub_date_str, '%Y-%m-%d').date()
+  except ValueError:
+    # If parsing fails, use a default date of 1900-01-01
+    pub_date = datetime.date(1900, 1, 1)
+  
+  return str(pub_date)
+
+def getThumbnail(ISBN):
+    response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q=isbn:{ISBN}")
+    data = response.json()
+    try:
+        thumbnail = data["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
+        response = requests.get(thumbnail)
+        thumbnail_image = Image.open(io.BytesIO(response.content))
+        return thumbnail_image
+    except:
+        return Image.open("default-thumbnail.jpeg")
+
+def getDes(ISBN):
+  response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q=isbn:{ISBN}")
+  data = response.json()
+  des = data["items"][0]["volumeInfo"]["description"]
+  return des
+
+def add_book():
+    isbn = isbn_entry.get()
+    quantity = quantity_entry.get()
+    # try:
+    title = getTitle(isbn)
+    author = getAuthor(isbn)
+    publication_date = getPubDate(isbn)
+    thumbnail = getThumbnail(isbn)
+    thumbnail = thumbnail.resize((300, 450))
+    thumbnail_image = ImageTk.PhotoImage(thumbnail)
+    query = ("INSERT INTO bookinventory (title, author, isbn, published_date, publisher, quantity, available_quantity, description)"
+                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
+    data = (title, author, isbn, publication_date, getPub(isbn), quantity, quantity, getDes(isbn))
+    cursor = mydb.cursor()
+    cursor.execute(query, data)
+    mydb.commit()
+    status_label.config(text="Book inserted successfully.", fg="green")
+    thumbnail_label.config(image=thumbnail_image)
+    thumbnail_label.image = thumbnail_image
+    # except:
+    #     status_label.config(text="Error inserting book", fg="red")
+
+
+def export_to_excel():
+    # Prompt user to select a location to save the file
+    filename = filedialog.asksaveasfilename(defaultextension=".xlsx", filetypes=(("Excel Files", "*.xlsx"),))
+
+    if filename:
+        query = "SELECT * FROM bookinventory"
+        df_bookinventory = pd.read_sql(query, mydb)
+
+        # Read data from the log table
+        query = "SELECT * FROM log"
+        df_log = pd.read_sql(query, mydb)
+
+        # Create a Pandas Excel writer using XlsxWriter as the engine
+        writer = pd.ExcelWriter(filename, engine='xlsxwriter')
+
+        # Write each dataframe to a different worksheet
+        df_bookinventory.to_excel(writer, sheet_name='Book Inventory', index=False)
+        df_log.to_excel(writer, sheet_name='Log', index=False)
+
+        # Close the Pandas Excel writer and MySQL connection
+        writer.save()
+
+# Define colors
+bg_color = "#FFFFFF"
+primary_color = "#FFA500"
+secondary_color = "#333333"
+
+# Create the main window
+root = Tk()
+root.title("Bentley Library")
+root.geometry("800x600")
+root.configure(bg=bg_color)
+
+# Create the header frame
+header_frame = Frame(root, bg=primary_color, pady=10)
+header_frame.pack(fill=X)
+
+# Create the header logo
+logo_image = Image.open("phoenix.png")
+logo_image = logo_image.resize((100, 100))
+logo_photo = ImageTk.PhotoImage(logo_image)
+logo_label = Label(header_frame, image=logo_photo, bg=primary_color)
+logo_label.pack(side=LEFT, padx=10)
+
+# Create the header title and subtitle
+title_label = Label(header_frame, text="Bentley Library", font=("Arial", 24, "bold"), fg=bg_color, bg=primary_color)
+title_label.pack(side=LEFT, padx=10)
+# subtitle_label = Label(header_frame, text="Explore, learn, and grow.", font=("Arial", 16), fg=bg_color, bg=primary_color)
+# subtitle_label.pack(side=LEFT, padx=10)
+
+# Create the main content frame
+content_frame = Frame(root, bg=bg_color, padx=50, pady=50)
+content_frame.pack(fill=BOTH, expand=True)
+
+# Create the ISBN label and entry
+isbn_label = Label(content_frame, text="ISBN", font=("Arial", 14), fg=secondary_color, bg=bg_color)
+isbn_label.pack()
+isbn_entry = Entry(content_frame, font=("Arial", 14))
+isbn_entry.pack()
+
+# Create the quantity label and entry
+quantity_label = Label(content_frame, text="Quantity", font=("Arial", 14), fg=secondary_color, bg=bg_color)
+quantity_label.pack()
+quantity_entry = Entry(content_frame, font=("Arial", 14))
+quantity_entry.pack()
+
+# Create the add book button
+add_book_button = Button(content_frame, text="Add Book", font=("Arial", 14), bg='#000000', fg='#000000', padx=10, pady=5, command=add_book)
+add_book_button.pack(pady=20)
+
+# Create the thumbnail label
+thumbnail_label = Label(content_frame, bg=bg_color)
+thumbnail_label.pack()
+
+# Create the status label
+status_label = Label(content_frame, font=("Arial", 14), fg=secondary_color, bg=bg_color)
+status_label.pack(pady=20)
+
+# Create a button to export the data to Excel
+export_button = Button(root, text="Export to Excel", command=export_to_excel)
+export_button.pack()
+
+# Run the main loop
+root.mainloop()
