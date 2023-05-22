ab8e959406171d5b9cbd30e31c2533edef87535b
Patrickjliu <liujpatrick@gmail.com>
2023-05-22 14:34:02 -0700

    Django search works

--- BentleyLibrary/core/models.py
+++ BentleyLibrary/core/models.py
@@ -1,16 +1,40 @@
 from django.db import models
 
-class BookInventory(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
+class Bookinventory(models.Model):
+    title = models.CharField(max_length=255, db_collation='utf8mb3_unicode_ci')
+    author = models.CharField(max_length=255, db_collation='utf8mb3_unicode_ci')
     isbn = models.CharField(max_length=13)
     published_date = models.DateField()
     publisher = models.CharField(max_length=255)
-    quantity = models.PositiveIntegerField()
-    available_quantity = models.PositiveIntegerField()
-    description = models.TextField()
+    quantity = models.IntegerField()
+    available_quantity = models.IntegerField()
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'bookinventory'
 
     def __str__(self):
         return self.title
 
-from core.models import BookInventory
+class Log(models.Model):
+    book = models.ForeignKey(Bookinventory, models.DO_NOTHING)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    publisher = models.CharField(max_length=255)
+    publication_date = models.DateField()
+    isbn = models.CharField(max_length=13)
+    borrower_first_name = models.CharField(max_length=255)
+    borrower_last_name = models.CharField(max_length=255)
+    borrower_email = models.CharField(max_length=255)
+    borrowed_date = models.DateField()
+    borrowed_time = models.TimeField()
+    returned_date = models.DateField(blank=True, null=True)
+    returned_time = models.TimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'log'
+
+
+# from core.models import BookInventory
--- BentleyLibrary/core/views.py
+++ BentleyLibrary/core/views.py
@@ -1,21 +1,48 @@
 from django.shortcuts import render
-from .models import BookInventory
+from .models import Bookinventory
 from django.db.models import Q
 
+from .models import Bookinventory
+
 def search_results(request):
-    query = request.GET.get('q')  # Update parameter name to 'q'
+    query = request.GET.get('q')
     if query:
-        results = BookInventory.objects.filter(
+        results = Bookinventory.objects.filter(
             Q(title__icontains=query) |
             Q(publisher__icontains=query) |
             Q(author__icontains=query) |
             Q(description__icontains=query)
         )
     else:
-        results = []
-    print("Query:", query)
-    print("Results:", results)
-    return render(request, 'search_results.html', {'results': results, 'query': query})
+        results = Bookinventory.objects.all()
+
+    context = {
+        'results': results,
+        'query': query
+    }
+    return render(request, 'search_results.html', context)
+
+
+# def search_results(request):
+    
+#     books = Bookinventory.objects.all()
+#     context = {
+#         'books' : books
+#     }
+#     return render(request, 'search_results.html', context)
+#     # query = request.GET.get('q')  # Update parameter name to 'q'
+#     # if query:
+#     #     results = BookInventory.objects.filter(
+#     #         Q(title__icontains=query) |
+#     #         Q(publisher__icontains=query) |
+#     #         Q(author__icontains=query) |
+#     #         Q(description__icontains=query)
+#     #     )
+#     # else:
+#     #     results = []
+#     # print("Query:", query)
+#     # print("Results:", results)
+#     # return render(request, 'search_results.html', {'results': results, 'query': query})
 
 
 def index(request):
